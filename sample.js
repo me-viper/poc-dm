86775003bc0e6318a14aed782d410192f4334f04
Andrew Kurochka <aki.viper@gmail.com>
2018-01-12 07:51:49 +0200

    Changes sample script

--- sample.js
+++ sample.js
@@ -1,11 +1,16 @@
-var playerAccount = "0xf17f52151EbEF6C7334FAD080c5704D77216b732";
+var managerAccount = web3.eth.accounts[0];
+var playerAccount = web3.eth.accounts[1]; //"0xf17f52151EbEF6C7334FAD080c5704D77216b732";
+
+console.log(managerAccount);
+console.log(playerAccount);
 
 var mc;
+ManagerContract.deployed().then(inst => mc = inst);
+mc.getCurrentState();
+mc.send(web3.toWei(10, "ether"));
+mc.signContractWithPlayer(playerAccount, "Some Player", web3.toWei(7, "ether"), {from: managerAccount});
 
-ManagerContract.deployed().then(inst => {
-    mc = inst;
-    mc.signContractWithPlayer.call(playerAccount, "Some Player", 5).then(r => {
-        console.log(r);
-        mc.getCurrentStatus.call().then(rr => console.log(rr));
-    });
-});
+web3.eth.getBalance(mc.address);
+
+mc.getCurrentState();
+mc.signContractWithManager({from: playerAccount});
